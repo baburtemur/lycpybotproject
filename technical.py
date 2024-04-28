f64b6616fb229a08fd3b021f4e74a440cd79dd08
baburtemur <zavalovila321@gmail.com>
2024-04-29 02:33:55 +0300

    xd

--- technical.py
+++ technical.py
@@ -1,7 +1,7 @@
 import inspect
 import sys
 
-TOKEN = "MTIzMTM0MjgzMzk2NTczMTkyMg.GuOeLV.YKkJTnqme_M4pnAJywwa9iooDctJJo5IxwfMAE"
+TOKEN = "MTIzMTM0MjgzMzk2NTczMTkyMg.GNvaPO.b8OqbZLlUvb5kqDB_K-EWJT_O5E1UQIzgB5pAo"
 YT_API = "AIzaSyDq4ROZColssRSxsQl1Rpt0weyrAb7uMrw"
 BOT_ID = 1231342833965731922
 BOT_AUTH_HEADER = "https://discord.com/oauth2/authorize"
